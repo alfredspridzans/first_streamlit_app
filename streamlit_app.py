6082490fe89480c6745878cc9ee4c41b238f6979
alfredspridzans <154338685+alfredspridzans@users.noreply.github.com>
2023-12-19 14:31:49 +0100

    Update streamlit_app.py
    🐔

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 streamlit.title('test')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+
+🥣 🥗 🐔 🥑🍞
